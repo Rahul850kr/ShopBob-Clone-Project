edc6ed353fc1d61441633cc36e3dd6c405a05655
Alisha <97678545+AlishaMohasin@users.noreply.github.com>
2022-05-05 14:51:39 +0530

    To check navbar and footer

---
+++ index.js
@@ -0,0 +1,11 @@
+// imported navbar;
+import { navbar } from "../components/navbar.js";
+let n=document.getElementById("navbar");
+n.innerHTML=navbar();
+
+// imported footer;
+import { footer } from "../components/footer.js";
+let n1=document.getElementById("footer");
+n1.innerHTML=footer();
+
+
