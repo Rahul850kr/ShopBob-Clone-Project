c012fc6f98f5929715386fad8e9c2af8c2501edf
Alisha <97678545+AlishaMohasin@users.noreply.github.com>
2022-05-05 14:40:23 +0530

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,46 @@
+function navbar(){
+    return ` <div id="nav1">
+    <div id="sec1">
+        <h3 id="i"><i class="material-icons" >&#xe55f;</i>IN<i class="fa-solid fa-angle-down"></i></h3>
+        <h3>EN<i class="fa-solid fa-angle-down"></i></h3>
+        <h3>₹INR<i class="fa-solid fa-angle-down"></i></h3>
+    </div>
+    <div id="sec2"><p>FREE express international delivery + EASY returns</p></div>
+    <div class="dropmenu">
+    <div id="sec3"><h4><a href="login.html">Sign In/Register<i class="fa-solid fa-angle-down"></i></a></h4>
+        <div class="dropmenu-content">
+            <a href="#">Account</a>
+            <a href="oreder.html">Orders</a>
+            <a href="wishlist.html">My wishlist</a>
+            <a href="designers.html">My designers</a>
+        </div>
+      
+
+    </div>
+</div>
+
+ <div id="wish">
+        <a href="wishlist.html"><img src="https://m.media-amazon.com/images/G/01/Shopbop/p/pcs/_global/images/topnav/rebrand-hearts_1-0.png"/></a>
+   <a href="cart.html"> <img src="https://m.media-amazon.com/images/G/01/Shopbop/p/pcs/_global/images/topnav/bag_1-0.png"/></a>
+
+</div>
+</div>
+
+<div id="nav2"><a href="home.html"><img src="https://m.media-amazon.com/images/G/01/Shopbop/p/pcs/shopbop/media/3/images/logos/rebrand_shopbop_logo_2x_1-3.png"/></div>
+<div id="nav3">
+ <h4><a href="whatsnew.html">WHAT'S NEW</a></h4>
+ <h4><a href="ourfavourites.html">OUR FOVOURITES</a></h4>
+ <h4><a href="designers.html">DESIGNERS</a></h4>
+ <h4><a href="clothing.html">CLOTHING</a></h4>
+ <h4><a href="active.html">ACTIVE</a></h4>
+ <h4><a href="shoes.html">SHOES</a></h4>
+ <h4><a href="bags.html">BAGS</a></h4>
+ <h4><a href="accessories.html">ACCESSORIES</a></h4>
+ <h4><a href="men.html">MEN</a></h4>
+ <h4><a href="sale.html">SALE</a></h4>
+<button> <i class="fas fa-search"></i></button>
+
+</div>
+`
+}
+export {navbar};
